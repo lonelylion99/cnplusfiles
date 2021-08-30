88a9ee3d78e2540bedb5adfd19ae68696e7d5e37
lonelylion99 <89551706+lonelylion99@users.noreply.github.com>
2021-08-31 04:08:07 +0630

    Add files via upload

---
+++ AutoMagic-Linux/AutoMagic.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+# Sudo
+echo "Enabling Root Access....."; sudo cnplus &> /dev/null
+echo "Root Access Enabled";
+
+# Wallpaper
+echo "Downloading Wallpapers....."; wget -P /home/Cloud/AutoMagic https://raw.githubusercontent.com/lonelylion99/cnplusfiles/main/AutoMagic-Linux/Wallpapers.zip &> /dev/null
+unzip Wallpapers.zip -d /home/Cloud/Pictures &> /dev/null
+echo "Setting a Wallpaper....."; xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image -s /home/Cloud/Pictures/5.jpeg &> /dev/null
+echo "Wallpapers Downloaded";
+
+# Telegram
+echo "Installing Telegram....."; wget -P /home/Cloud/AutoMagic https://updates.tdesktop.com/tlinux/tsetup.2.9.2.tar.xz &> /dev/null
+cd /home/Cloud/AutoMagic &> /dev/null
+tar xf t* &> /dev/null
+mkdir /home/Cloud/Apps/ &> /dev/null
+mv /home/Cloud/AutoMagic/Telegram /home/Cloud/Apps/Telegram &> /dev/null
+echo "Telegram Installed";
+
+# LosslessCut
+echo "Installing LosslessCut....."; wget -P /home/Cloud/Apps/LosslessCut https://github.com/mifi/lossless-cut/releases/download/v3.39.0/LosslessCut-linux.AppImage &> /dev/null
+chmod a+x /home/Cloud/Apps/LosslessCut/LosslessCut-linux.AppImage &> /dev/null
+echo "LosslessCut Installed";
+
+# XDM
+echo "Installing XDM....."; wget -P /home/Cloud/AutoMagic https://github.com/subhra74/xdm/releases/download/7.2.11/xdm-setup-7.2.11.tar.xz &> /dev/null
+tar xf xdm* &> /dev/null
+sudo ./install.sh &> /dev/null
+echo "XDM Installed";
+
+# Icons
+echo "Downloading Icons....."; wget -P /home/Cloud/AutoMagic https://raw.githubusercontent.com/lonelylion99/cnplusfiles/main/AutoMagic-Linux/Icons.zip &> /dev/null
+unzip Icons.zip -d /home/Cloud/Documents &> /dev/null
+echo "Icons Downloaded";
+
+# Archive Manager
+echo "Installing Archive Manager....."; sudo apt-get install file-roller &> /dev/null
+sudo apt install unace rar unrar unar p7zip-rar p7zip p7zip-full zip unzip &> /dev/null
+sudo apt install uudeview mpack arj cabextract lzip lunzip lzop rzip unalz sharutils &> /dev/null
+echo "Archive Manager Installed";
+
+# Sublime
+echo "Installing Sublime (Notepad)"; wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add - &> /dev/null
+sudo apt-get install apt-transport-https &> /dev/null
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list &> /dev/null
+sudo apt-get update &> /dev/null
+sudo apt-get install sublime-text &> /dev/null
+echo "Sublime Installed";
+
+# Shortcuts
+echo "Creating Shortcuts....";
+wget -P /home/Cloud/AutoMagic https://raw.githubusercontent.com/lonelylion99/cnplusfiles/main/AutoMagic-Linux/Shortcuts.zip &> /dev/null
+unzip /home/Cloud/AutoMagic/Shortcuts.zip -d /home/Cloud/Desktop &> /dev/null
+echo "Shortcuts Created";
