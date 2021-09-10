9943c04f748297243756debd284888a6a5e931b1
lonelylion99 <89551706+lonelylion99@users.noreply.github.com>
2021-09-10 08:01:22 +0630

    Update AutoMagic.sh

--- AutoMagic-Linux/AutoMagic.sh
+++ AutoMagic-Linux/AutoMagic.sh
@@ -15,11 +15,9 @@ wget -P /home/Cloud/Documents/Icons https://raw.githubusercontent.com/lonelylion
 wget -P /home/Cloud/Desktop https://raw.githubusercontent.com/lonelylion99/cnplusfiles/main/AutoMagic-Linux/Shortcuts/google-chrome.desktop &> /dev/null &&  chmod a+x /home/Cloud/Desktop/google-chrome.desktop
 echo "___________________________________________________________"
 echo "Installing Firefox.....";
-echo "...downloading"; wget --content-disposition --directory-prefix=/home/Cloud/AutoMagic https://download-installer.cdn.mozilla.net/pub/firefox/releases/92.0/linux-x86_64/en-US/firefox-92.0.tar.bz2  &> /dev/null
+echo "...downloading"; wget -P /home/Cloud/AutoMagic https://download-installer.cdn.mozilla.net/pub/firefox/releases/92.0/linux-x86_64/en-US/firefox-92.0.tar.bz2  &> /dev/null
 echo "...extracting"; tar xjf /home/Cloud/AutoMagic/firefox-*.tar.bz2 -C /home/Cloud/Apps  &> /dev/null
-echo "...done";
-wget -P /home/Cloud/Documents/Icons https://raw.githubusercontent.com/lonelylion99/cnplusfiles/main/AutoMagic-Linux/Icons/Firefox.png &> /dev/null
-wget -P /home/Cloud/Desktop https://raw.githubusercontent.com/lonelylion99/cnplusfiles/main/AutoMagic-Linux/Shortcuts/Firefox.desktop &> /dev/null &&  chmod a+x /home/Cloud/Desktop/Firefox.desktop
+echo "...done"; wget -P /home/Cloud/Documents/Icons https://raw.githubusercontent.com/lonelylion99/cnplusfiles/main/AutoMagic-Linux/Icons/Firefox.png &> /dev/null && wget -P /home/Cloud/Desktop https://raw.githubusercontent.com/lonelylion99/cnplusfiles/main/AutoMagic-Linux/Shortcuts/Firefox.desktop &> /dev/null && chmod a+x /home/Cloud/Desktop/Firefox.desktop
 echo "___________________________________________________________"
 echo "Installing Chromium Browser....."; sudo apt install chromium-browser &> /dev/null
 wget -P /home/Cloud/Documents/Icons https://raw.githubusercontent.com/lonelylion99/cnplusfiles/main/AutoMagic-Linux/Icons/Chromium.png &> /dev/null
