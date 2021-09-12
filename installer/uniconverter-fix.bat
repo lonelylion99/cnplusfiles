1f8eaec6b840909303b0746892bb63d712bfb08a
lonelylion99 <89551706+lonelylion99@users.noreply.github.com>
2021-09-13 03:30:38 +0630

    Update uniconverter-fix.bat

--- installer/uniconverter-fix.bat
+++ installer/uniconverter-fix.bat
@@ -1,1 +1,6 @@
+@echo off
 
+taskkill /IM VideoConverterUltimate.exe /F
+PowerShell Set-MpPreference -DisableRealtimeMonitoring $true
+PowerShell Set-MpPreference -DisableRealtimeMonitoring $false
+START "C:\Program Files\Wondershare\UniConverter\WondershareConverterUltimate.exe"
