370d98c5ebb0bc0cd57de4c31734f1730987a8aa
lonelylion99 <89551706+lonelylion99@users.noreply.github.com>
2021-09-13 04:02:02 +0630

    Update uniconverter-fix.bat

--- installer/uniconverter-fix.bat
+++ installer/uniconverter-fix.bat
@@ -2,5 +2,5 @@
 
 taskkill /IM VideoConverterUltimate.exe /F
 PowerShell Set-MpPreference -DisableRealtimeMonitoring $true
-timeout /t 3
+timeout /t 3 /nobreak
 PowerShell Set-MpPreference -DisableRealtimeMonitoring $false
