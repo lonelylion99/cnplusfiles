0b9a736b93a24fc26792fd4610f2f2e82afa5667
lonelylion99 <89551706+lonelylion99@users.noreply.github.com>
2021-09-13 03:46:12 +0630

    Update uniconverter-fix.ps1

--- installer/uniconverter-fix.ps1
+++ installer/uniconverter-fix.ps1
@@ -1,1 +1,4 @@
-
+Stop-Process -Name "VideoConverterUltimate"
+Set-MpPreference -DisableRealtimeMonitoring $true
+Set-MpPreference -DisableRealtimeMonitoring $false
+Start-Process "C:\Program Files\Wondershare\UniConverter\VideoConverterUltimate.exe"
