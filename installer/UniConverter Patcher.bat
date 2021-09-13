c8f00995b481d475bf0a601174ef0d49aef1a662
lonelylion99 <89551706+lonelylion99@users.noreply.github.com>
2021-09-13 14:57:44 +0630

    Update UniConverter Patcher.bat

--- installer/UniConverter Patcher.bat
+++ installer/UniConverter Patcher.bat
@@ -1,5 +1,5 @@
 @echo off
 
-START "C:\Program Files\Wondershare\UniConverter\Patcher.exe"
+START "" "C:\Program Files\Wondershare\UniConverter\Patcher.exe"
 DEL /f "C:\Users\kiyoe\Desktop\UniConverter Patcher.lnk"
 del %0
