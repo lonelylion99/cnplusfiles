c2e0b14f4259b0981407762b4ed4bdcef8fa6184
lonelylion99 <89551706+lonelylion99@users.noreply.github.com>
2021-09-14 07:17:01 +0630

    Update xfce4.sh

--- xfce4.sh
+++ xfce4.sh
@@ -1,4 +1,4 @@
-echo "Ngrok အရင်ဒေါင်းမေ..."
+echo "Ngrok ဒေါင်းမေ..."
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip.1 > /dev/null 2>&1
