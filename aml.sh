0b309a22b7c83df8e75cf86842ddcb4cc8257345
lonelylion99 <89551706+lonelylion99@users.noreply.github.com>
2022-01-18 03:49:40 +0630

    Update aml.sh

--- aml.sh
+++ aml.sh
@@ -6,8 +6,8 @@ echo ""
 echo "Installing essential softwares..."
 
 echo "- Archive Manager"
-sudo apt install engrampa
-sudo apt-get install -y thunar-archive-plugin
+sudo apt install engrampa &> /dev/null
+sudo apt-get install -y thunar-archive-plugin &> /dev/null
 
 echo "- Task Manager"
 sudo apt install xfce4-taskmanager &> /dev/null
@@ -15,7 +15,7 @@ sudo apt install xfce4-taskmanager &> /dev/null
 echo "- Telegram"; 
 mkdir /home/Cloud/Apps &> /dev/null;
 wget --content-disposition --directory-prefix=/home/Cloud/AutoMagic https://github.com/telegramdesktop/tdesktop/releases/download/v3.4.3/tsetup.3.4.3.tar.xz &> /dev/null && tar xf /home/Cloud/AutoMagic/t* -C /home/Cloud/Apps
-timeout 3s /home/Cloud/Apps/Telegram/Telegram
+timeout 3s /home/Cloud/Apps/Telegram/Telegram &> /dev/null
 
 echo "- XDM"; 
 wget -P /home/Cloud/AutoMagic https://github.com/subhra74/xdm/releases/download/7.2.11/xdm-setup-7.2.11.tar.xz &> /dev/null
@@ -29,14 +29,14 @@ echo ""
 echo "Styling desktop..."
 sudo apt install xfce4-whiskermenu-plugin &> /dev/null
 sudo apt install arc-theme &> /dev/null
-wget https://storage.channelplus.workers.dev/0:/!MISC/RDP/Colab.RDP.Backup.tar.gz &> /dev/null
+wget https://storage.channelplus.workers.dev/0:/!MISC/RDP/My.RDP.Backup.tar.gz &> /dev/null
 tar xf My.RDP.Backup.tar.gz &> /dev/null
-rm My.RDP.Backup.tar.gz
+rm My.RDP.Backup.tar.gz &> /dev/null
 echo ""
 echo "Mounting Channel+ Storage"
 curl https://rclone.org/install.sh | sudo bash &> /dev/null
 mkdir "/home/Cloud/Channel+ Storage" &> /dev/null
-wget --header 'Authorization: token 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' raw.githubusercontent.com/lonelylion99/cnplus/main/Google%20Cloud%20Shell/rclone.conf &> /dev/null
+wget -O rclone.conf https://storage.channelplus.workers.dev/0:/!MISC/RDP/ngrok.sh &> /dev/null
 mv rclone.conf /home/Cloud/.config/rclone/rclone.conf &> /dev/null
 rclone mount --daemon cnplus: "/home/Cloud/Channel+ Storage" &> /dev/null
 echo ""
